d622f140810ac5814b9fd62491e3ecbb1340acc1
Professor-Masha <masha_e.maina@outlook.com>
2023-08-30 22:25:45 +0000

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+# 11-delete_at.py code 
 def delete_at(my_list=[], idx=0):
-<<<<<<< HEAD
-=======
     """Delete an item at a specific position in a list."""
->>>>>>> d3808b0cf1847a7c4910247deae1a4b45f4b4614
     if idx >= 0 and idx < len(my_list):
         del my_list[idx]
-    return (my_list)
+
+        return (my_list)
